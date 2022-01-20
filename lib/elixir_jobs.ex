625b7183c2c7e286cd90b66855ec25f0cc312cec
Kozlov Ilya <ilya@kozlov.dev>
2022-01-20 20:07:26 +0600

    refactor: remove dead code

--- lib/elixir_jobs.ex
+++ lib/elixir_jobs.ex
@@ -1,18 +1,3 @@
 defmodule ElixirJobs do
-  @moduledoc """
-  Documentation for `ElixirJobs`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirJobs.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/elixir_jobs_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ElixirJobsTest do
-  use ExUnit.Case
-  doctest ElixirJobs
-
-  test "greets the world" do
-    assert ElixirJobs.hello() == :world
-  end
-end
